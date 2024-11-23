def1b9768e35ca000474cf6dd85be3bef9cf66fb
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-23 10:39:27 +0100

    maj

---
+++ src/assets/js/browser.js
@@ -0,0 +1,2 @@
+// S'éxécute dans la page web du navigateur et pas dans la page de l'extension!
+console.log('File "browser.js" importé avec succès!');
--- src/popup/content.js
+++
@@ -1,2 +0,0 @@
-// S'éxécute dans la page web et pas dans la page de l'extension
-console.log('File "content.js" importé avec succès!');
