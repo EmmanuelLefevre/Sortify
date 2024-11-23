9ab389e1ca1623b0c0a6fce9878621b34d60f2c4
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-23 08:12:54 +0100

    maj

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,6 @@
 // Gère la logique JavaScript pour l'interface utilisateur du popup.
 
-document.getElementById("sortBtn").addEventListener("click", () => {
+document.getElementById("sort-btn").addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "sortBookmarks" }, (response) => {
     if (response.success) {
       alert("Favoris triés avec succès !");
