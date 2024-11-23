702c70559ca2925982ff200465990331dbe0035d
Emmanuel Lefevre <waiasiaa@protonmail.com>
2024-11-23 13:51:18 +0100

    maj

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 console.log('Service worker api.js démarré avec succès!');
 
-chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-  if (message.action === "sortBookmarks") {
-    chrome.bookmarks.getTree((bookmarks) => {
-      console.log("Favoris triés", bookmarks);
-      sendResponse({ success: true });
-    });
-    return true; // Indique que la réponse est asynchrone
-  }
-});
+// chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+//   if (message.action === "sortBookmarks") {
+//     chrome.bookmarks.getTree((bookmarks) => {
+//       console.log("Favoris triés", bookmarks);
+//       sendResponse({ success: true });
+//     });
+//     return true; // Indique que la réponse est asynchrone
+//   }
+// });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,31 +1,43 @@
 // Chargement de l'extension
-document.addEventListener("DOMContentLoaded", () => {
-  const button  = document.getElementById("enable-notifs");
-
-  button .addEventListener("click", () => {
-    if (Notification.permission === "granted") {
-      console.log("Autorisation pour les notifications déjà activée.");
-    }
-    else if (Notification.permission === "denied") {
-      alert("Veuillez activer les notifications dans les paramètres du navigateur!");
-    }
-    else {
-      // L'état est "default", demander la permission
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          console.log("Autorisation pour les notifications activée.");
-        }
-        else {
-          console.log("Autorisation pour les notifications refusée.");
-        }
+document.addEventListener('DOMContentLoaded', () => {
+  // Vérifer si le navigateur prend en charge les notifications
+  if (!("Notification" in window)) {
+    alert("Ce navigateur ne prend pas en charge les notifications de bureau!");
+  }
+  // Vérifier l'état des permissions de notifications
+  else if (Notification.permission === "granted") {
+    new Notification('Test',{
+      body : '🚀🚀🚀 Notifications déjà activées! 🚀🚀🚀'
+    });
+  }
+  // Si la permission n'est pas encore déterminée, demander à l'utilisateur
+  else if (Notification.permission === "default") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        new Notification('Test', {
+          body: '🚀🚀🚀 Notifications activées! 🚀🚀🚀'
+        });
+      }
+      else {
+        new Notification('Test', {
+          body: '🤬🤬🤬 Notifications refusées! 🤬🤬🤬'
+        });
+      }
+    }).catch((err) => {
+      new Notification('Test', {
+        body: '⚠️ Erreur lors de la demande des permissions! ⚠️'
       });
-    }
-  });
+      console.error("Erreur lors de la demande des permissions : ", err);
+    });
+  }
+  // Spamming
+  else if (Notification.permission === "denied") {
+    alert("Veuillez activer les notifications dans les paramètres du navigateur svp!");
+  }
 });
 
 
-// Sort button click
-document.getElementById("sort-btn").addEventListener("click", () => {
+document.getElementById('sort-btn').addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: "sortBookmarks" }, (response) => {
     if (response.success) {
       console.log("Favoris triés avec succès !");
