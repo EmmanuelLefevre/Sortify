577373bfb0d03bea41fc48b0a1291cfbc1259573
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-23 10:02:24 +0100

    maj

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,14 +1,30 @@
-// Gère la logique JavaScript pour l'interface utilisateur du popup.
-
-document.getElementById("sort-btn").addEventListener("click", function() {
-  console.log('Fichier popup.js chargé avec succès!');
+// Chargement de l'extension
+document.addEventListener("DOMContentLoaded", () => {
+  // Vérifier l'état des permissions de notifications
+  if (Notification.permission === "granted") {
+    console.log("Autorisation pour les notifications déjà activée.");
+  }
+  else if (Notification.permission === "denied") {
+    alert("Veuillez activer les notifications dans les paramètres du navigateur!");
+  }
+  else {
+    // L'état est "default", demander la permission
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("Autorisation pour les notifications activée.");
+      }
+      else {
+        console.log("Autorisation pour les notifications refusée.");
+      }
+    });
+  }
 });
 
-
+// Sort button click
 document.getElementById("sort-btn").addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "sortBookmarks" }, (response) => {
     if (response.success) {
-      alert("Favoris triés avec succès !");
+      console.log("Favoris triés avec succès !");
     }
   });
 });
