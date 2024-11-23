96fc1236b194d53c18e1b093c5504175f01148f8
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-23 10:59:51 +0100

    maj

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,24 +1,28 @@
 // Chargement de l'extension
 document.addEventListener("DOMContentLoaded", () => {
-  // Vérifier l'état des permissions de notifications
-  if (Notification.permission === "granted") {
-    console.log("Autorisation pour les notifications déjà activée.");
-  }
-  else if (Notification.permission === "denied") {
-    alert("Veuillez activer les notifications dans les paramètres du navigateur!");
-  }
-  else {
-    // L'état est "default", demander la permission
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        console.log("Autorisation pour les notifications activée.");
-      }
-      else {
-        console.log("Autorisation pour les notifications refusée.");
-      }
-    });
-  }
+  const button  = document.getElementById("enable-notifs");
+
+  button .addEventListener("click", () => {
+    if (Notification.permission === "granted") {
+      console.log("Autorisation pour les notifications déjà activée.");
+    }
+    else if (Notification.permission === "denied") {
+      alert("Veuillez activer les notifications dans les paramètres du navigateur!");
+    }
+    else {
+      // L'état est "default", demander la permission
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("Autorisation pour les notifications activée.");
+        }
+        else {
+          console.log("Autorisation pour les notifications refusée.");
+        }
+      });
+    }
+  });
 });
+
 
 // Sort button click
 document.getElementById("sort-btn").addEventListener("click", () => {
